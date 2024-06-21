5891d4b7bc8cfd6b6069efd7ab32d43e9ed7568a
Nelson Neto <nelson.ash@outlook.com>
2024-06-21 17:21:35 -0300

    Testable api

---
+++ Management.Domain.Tests/PagamentoUnitTest.cs
@@ -0,0 +1,178 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Moq;
+using System.Net;
+
+namespace Management.Domain.Tests;
+
+//[Trait("Category", "UnitTests")]
+//public class PagamentoCommandUnitTest
+//{
+//    [Fact]
+//    public void Pagamento()
+//    {
+//    }
+//}
+
+[Trait("Category", "UnitTests")]
+public class PagamentoControllerUnitTest
+{
+    private readonly IFixture _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
+    private readonly PagamentoController _sut;
+    private readonly PagamentoCommand _command;
+    private readonly CancellationToken _token = CancellationToken.None;
+    private readonly Mock<IPagamentoHandler> _mockHandler;
+
+    public PagamentoControllerUnitTest()
+    {
+        _mockHandler = _fixture.Freeze<Mock<IPagamentoHandler>>();
+        _fixture.Freeze<Mock<IPagamentoHandler>>()
+                .Setup(x => x.Handler(It.IsAny<PagamentoCommand>(), _token))
+                .ReturnsAsync(new Pagamento());
+
+        _command = _fixture.Create<PagamentoCommand>();
+        _sut = _fixture.Create<PagamentoController>();
+    }
+
+    [Fact]
+    public async Task Dado_Commando_Valido_Retorna_Created()
+    {
+        var result = await _sut.Post(_command, _token);
+        result.Should().Be(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task Dado_Exception_Retorna_Created()
+    {
+        _fixture.Freeze<Mock<IPagamentoHandler>>()
+                .Setup(x => x.Handler(It.IsAny<PagamentoCommand>(), _token))
+                .ThrowsAsync(new NotImplementedException("whatever"));
+        var result = await _sut.Post(_command, _token);
+        result.Should().Be(HttpStatusCode.InternalServerError);
+    }
+}
+
+[Trait("Category", "UnitTests")]
+public class PagamentoHandlerUnitTest
+{
+    private readonly IFixture _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
+    private readonly PagamentoHandler _sut;
+    private readonly PagamentoCommand _command;
+    private readonly CancellationToken _token = CancellationToken.None;
+    private readonly Mock<IPagamentoRepository> _repositoryMock;
+
+    public PagamentoHandlerUnitTest()
+    {
+        _repositoryMock = _fixture.Freeze<Mock<IPagamentoRepository>>();
+        _command = _fixture.Create<PagamentoCommand>();
+        _sut = _fixture.Create<PagamentoHandler>();
+    }
+
+    [Fact]
+    public async void Dado_Comando_Inserir_Corretamente_Retorna_Pagamento_Inserido()
+    {
+        var result = await _sut.Handler(_command, _token);
+        result.Should().NotBeNull();
+        result.Id.Should().NotBe(0);
+        result.NumeroContrato.Should().NotBe(_command.NumeroContrato);
+    }
+
+    [Fact]
+    public async void Dado_Comando_Inserir_Duplicidade_Retorna_Pagamento_Inserido()
+    {
+        _fixture.Freeze<Mock<IPagamentoRepository>>()
+                .Setup(x => x.Inserir(It.IsAny<Pagamento>(), _token))
+                .ThrowsAsync(new NotImplementedException("whatever"));
+        Func<Task> result = async () => await _sut.Handler(_command, _token);
+        await result.Should().ThrowAsync<NotImplementedException>()
+            .WithMessage("whatever");
+    }
+}
+
+[Trait("Category", "UnitTests")]
+public class PagamentoRepositoryUnitTest
+{
+    private readonly IFixture _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
+    private readonly PagamentoRepository _sut;
+    private readonly Pagamento _entity;
+    private readonly CancellationToken _token = CancellationToken.None;
+
+    public PagamentoRepositoryUnitTest()
+    {
+        _entity = _fixture.Build<Pagamento>()
+            .With(x => x.Id, 0)
+            .Create();
+        _sut = _fixture.Create<PagamentoRepository>();
+    }
+
+    [Fact]
+    public async Task Insercao_Valida_Retorna_Entitidade_Criada()
+    {
+        var result = await _sut.Inserir(_entity, _token);
+        result.Should().BeSameAs(_entity);
+    }
+
+    [Fact]
+    public async Task Insercao_Invalida_Se_Id_Diferente_Zero()
+    {
+        _entity.Id = 1;
+        Func<Task> result = async () => await _sut.Inserir(_entity, _token);
+        await result.Should().ThrowAsync<NotImplementedException>()
+            .WithMessage("whatever");
+    }
+}
+
+//[Trait("Category", "UnitTests")]
+//public class PagamentoUnitTest
+//{
+//    [Fact]
+//    public void Pagamento()
+//    {
+//    }
+//}
+
+//[Trait("Category", "UnitTests")]
+//public class PagamentoEventUnitTest
+//{
+//    [Fact]
+//    public void Pagamento()
+//    {
+//    }
+//}
+
+//public record PagamentoEvent(int Id, string NumeroContrato, decimal Preco, int Quantidade, int IdEstoque);
+
+//[Trait("Category", "UnitTests")]
+//public class PagamentoConsumerUnitTest
+//{
+//    [Fact]
+//    public void Test1()
+//    {
+//    }
+//}
+
+//public class PagamentoConsumer
+//{
+//    public async Task<Pagamento> Inserir(CancellationToken cancellationToken = default)
+//    {
+//        return await Task.FromResult(new Pagamento());
+//    }
+//}
+
+//[Trait("Category", "UnitTests")]
+//public class PagamentoPublisherUnitTest
+//{
+//    [Fact]
+//    public void Test1()
+//    {
+//    }
+//}
+
+//public class PagamentoPublisher
+//{
+//    public async Task<Pagamento> Inserir(CancellationToken cancellationToken = default)
+//    {
+//        return await Task.FromResult(new Pagamento());
+//    }
+//}
---
+++ Managemt.Api/Features/Pagamentos/Pagamento.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using System.Net;
+
+namespace Managemt.Api.Features.Pagamentos;
+
+public class PagamentoMapping : Profile
+{
+    public PagamentoMapping()
+    {
+        CreateMap<PagamentoCommand, Pagamento>();
+    }
+}
+
+public class Pagamento
+{
+    public int Id { get; set; }
+    public string? NumeroContrato { get; set; }
+    public decimal Preco { get; set; }
+    public int Quantidade { get; set; }
+    public int IdEstoque { get; set; }
+}
+
+public record PagamentoCommand(string NumeroContrato, decimal Preco, int Quantidade);
+
+public interface IPagamentoRepository
+{
+    Task<Pagamento> Inserir(Pagamento entity, CancellationToken cancellationToken = default);
+}
+
+public class PagamentoRepository : IPagamentoRepository
+{
+    public async Task<Pagamento> Inserir(Pagamento entity, CancellationToken cancellationToken = default)
+    {
+        if (entity.Id != 0) throw new NotImplementedException("whatever");
+        return await Task.FromResult(entity);
+    }
+}
+
+public interface IPagamentoHandler
+{
+    Task<Pagamento> Handler(PagamentoCommand command, CancellationToken cancellationToken = default);
+}
+
+public class PagamentoHandler : IPagamentoHandler
+{
+    private readonly IPagamentoRepository _pagamentoRepository;
+
+    public PagamentoHandler(IPagamentoRepository pagamentoRepository)
+    {
+        _pagamentoRepository = pagamentoRepository;
+    }
+
+    public async Task<Pagamento> Handler(PagamentoCommand command, CancellationToken cancellationToken = default)
+    {
+        //TODO: Pagamento entity = command.MapTo<Pagamento>();
+        Pagamento entity = new Pagamento() { NumeroContrato = command.NumeroContrato, Preco = command.Preco, Quantidade = command.Quantidade };
+        var result = await _pagamentoRepository.Inserir(entity, cancellationToken);
+        return await Task.FromResult(result);
+    }
+}
+
+public class PagamentoController
+{
+    private readonly IPagamentoHandler _pagamentoHandler;
+
+    public PagamentoController(IPagamentoHandler pagamentoHandler)
+    {
+        _pagamentoHandler = pagamentoHandler;
+    }
+
+    public async Task<HttpStatusCode> Post(PagamentoCommand command, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            Pagamento pagamento = await _pagamentoHandler.Handler(command, cancellationToken);
+            return HttpStatusCode.Created;
+        }
+        catch (Exception)
+        {
+            return HttpStatusCode.InternalServerError;
+        }
+    }
+}
